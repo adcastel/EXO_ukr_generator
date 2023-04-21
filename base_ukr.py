b6c5704700f6861d558b814761218b3ed5fdd9eb
Adrián Castelló <adcastel@uji.es>
2023-04-21 12:37:00 +0200

    C reg completed

--- base_ukr.py
+++ base_ukr.py
@@ -46,49 +46,43 @@ def set_windowing(p):
 def split_loop(p, loop, LANE):
     return divide_loop(p,loop, LANE, ['{}t'.format(loop),'{}tt'.format(loop)], perfect=True)
 
+def vectorial_memory(p, var, mem):
+    p = set_memory(p, var, mem)
+    return p
+
+def from_C_to_Creg_2d(p, MR, NR, LANE):
+      Cp = 'C[{} * jt + jtt, {} * it + itt]'.format(LANE,LANE)
+      p = stage_mem(p, 'C[_] += _', Cp, 'C_reg')
+      p = expand_dim(p, 'C_reg', LANE, 'itt', unsafe_disable_checks=True)
+      p = expand_dim(p, 'C_reg', MR//LANE, 'it', unsafe_disable_checks=True)
+      p = expand_dim(p, 'C_reg', NR, 'jt*4+jtt', unsafe_disable_checks=True)
+      p = lift_alloc(p, 'C_reg', n_lifts=5)
+      p = autofission(p, p.find('C_reg[_] = _').after(), n_lifts=5)
+      p = autofission(p, p.find('C[_] = _').before(), n_lifts=5)
+      #TODO: read instructions and type from file
+      p = replace(p, 'for itt in _: _ #0', neon_vld_4xf32)
+      p = replace(p, 'for itt in _: _ #1', neon_vst_4xf32)
+      p = vectorial_memory(p, 'C_reg', Neon)
+      return  p
+
 def generate_optimized_ukr(MR, NR, KC, LANE, windowing = 0):
       p = simplify(ukernel_get_ref(MR,NR))
       if windowing:
           p = rename(p, "uk_wind_{}x{}".format(MR,NR))
       else:
           p = rename(p, "uk_{}x{}".format(MR,NR))
+      #loops partition
       p = split_loop(p, 'i', LANE)
       p = split_loop(p, 'j', LANE)
 
-      Cp = 'C[{} * jt + jtt, {} * it + itt]'.format(LANE,LANE)
+      # C 
+      p = from_C_to_Creg_2d(p, MR, NR, LANE)
       return p
 
 
 
 """
-p = set_window(p, "A", True)
-p = set_window(p, "B", True)
-p = set_window(p, "C", True)
-
-print("windowing\n",p)
-
-p = reorder_loops(p,'j k')
-p = reorder_loops(p,'i k')
-p = reorder_loops(p,'i j')
-#p = divide_loop(p,'j', LANE, ['jt','jtt'], perfect=True)
-#p = divide_loop(p,'i', LANE, ['it','itt'], perfect=True)
-p = divide_loop(p,'j', LANE, ['jt','jtt'], tail="cut")
-p = divide_loop(p,'i', LANE, ['it','itt'], tail="cut")
-print("Loop order\n",p)
-kk = 'C[{} * jt + jtt, {} * it + itt]'.format(LANE,LANE)
-
-p = stage_mem(p, 'C[_] += _', kk, 'C_reg')
-p = expand_dim(p, 'C_reg', LANE, 'itt', unsafe_disable_checks=True)
-NO=NR//LANE
-AFAC=MR//LANE
-p = expand_dim(p, 'C_reg', AFAC, 'it', unsafe_disable_checks=True)
-p = expand_dim(p, 'C_reg', NR, 'jt*4+jtt', unsafe_disable_checks=True)
-p = lift_alloc(p, 'C_reg', n_lifts=5)
-p = autofission(p, p.find('C_reg[_] = _').after(), n_lifts=7)
-p = autofission(p, p.find('C[_] = _').before(), n_lifts=7)
 print("C matrix\n",p)
-p = replace(p, 'for itt in _: _ #0', neon_vld_4xf32)
-p = replace(p, 'for itt in _: _ #1', neon_vst_4xf32)
 p = set_memory(p, 'C_reg', Neon4f)
 print("C matrix\n",p)
 
@@ -101,7 +95,6 @@ p = bind_expr(p, f'{buf}[_]', f'{buf}_vec')
 p = expand_dim(p, f'{buf}_vec', LANE, 'itt', unsafe_disable_checks=True)
 p = expand_dim(p, f'{buf}_vec', AFAC, 'it', unsafe_disable_checks=True)
 p = lift_alloc(p, f'{buf}_vec', n_lifts=5)
-p = autofission(p, p.find(f'{buf}_vec[_] = _').after(), n_lifts=4)
 #p = autofission(p, p.find(f'{buf}_vec[_] = _').after(), n_lifts=3)
 p = replace(p, 'for itt in _: _ #0', neon_vld_4xf32)
 p = set_memory(p, f'{buf}_vec', Neon4f)
