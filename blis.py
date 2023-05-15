520f98896ded10b02b7648c7d08a181b38af0d0c
Adrián Castelló <adcastel@uji.es>
2023-05-15 11:35:13 +0200

    generator cleaned

--- blis.py
+++ blis.py
@@ -7,11 +7,13 @@ KC = 512
 LANE = 4
 alpha1=True
 beta1=True
-s = generate_original_ukr(MR, NR, KC, True, True)
-p = generate_original_ukr(MR, NR, KC, True, False)
 q = generate_optimized_ukr(MR, NR, KC, alpha1, beta1, LANE)
 print(q)
 
-beta1=False
-w = generate_optimized_ukr(MR, NR, KC, alpha1, beta1, LANE) #windowing version
-print(w)
+# if we want to use windowed feature for C
+x = generate_optimized_ukr(MR, NR, KC, alpha1, beta1, LANE,windowing=1) #windowing version
+print(x)
+
+# if we want use fp16 
+#w = generate_optimized_ukr(MR, NR, KC, alpha1, beta1, LANE,windowing=1, data="f16") #windowing version
+#print(w)
